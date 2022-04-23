0205721862589cb31588ee2d488d8efe09f549ea
MiraKalelova <94729054+MiraKalelova@users.noreply.github.com>
2022-04-23 09:55:55 +0600

    Add files via upload

---
+++ SignUp/AccessDeniedException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class AccessDeniedException extends Exception {
+    public AccessDeniedException(String message) { super(message); }
+}
---
+++ SignUp/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {super(message); }
+}
